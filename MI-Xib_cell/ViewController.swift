8b1e40258e1da6371058f5e8a5aa2eb48c60d78a
Ineta Magone <ineta.magone@accenture.com>
2022-02-09 20:00:18 +0200

    Initial Commit

---
+++ MI-Xib_cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MI-Xib_cell
+//
+//  Created by ineta.magone on 09/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
